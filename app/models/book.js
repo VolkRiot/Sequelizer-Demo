a1dc1f03e1b730eccab26ea18d717caecc56d680
Mikhail Metrikin <metrikin@gmail.com>
2017-05-01 23:17:10 -0700

    Sequilizer Demo

---
+++ app/models/book.js
@@ -0,0 +1,22 @@
+// Dependencies
+// =============================================================
+
+var Sequelize = require("sequelize");
+var seq = require('../config/connection.js');
+
+// Create a "Book" model with the following configuration
+
+var book = seq.define('book',{
+  title: Sequelize.STRING,
+  author: Sequelize.STRING,
+  genre: Sequelize.STRING,
+  page: Sequelize.INTEGER,
+  timestamps: false
+});
+
+book.sync().then(resp =>{
+  console.log("Success!");
+});
+
+module.exports = book;
+// Export the book model for other files to use
