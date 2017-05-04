1e72a0770ae375703d73369ac72e859e0090756e
Mikhail Metrikin <metrikin@gmail.com>
2017-05-03 19:17:24 -0700

    Final Working

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -13,42 +13,91 @@ module.exports = function(app) {
 
   // Add sequelize code to get all books and return them as JSON
   app.get("/api/all", function(req, res) {
-
+    Book.findAll().then(all =>{
+      res.json(all);
+    })
   });
 
   // Add sequelize code to get a specific book and return it as JSON
   app.get("/api/:book", function(req, res) {
 
+    Book.findAll({
+      where: {
+        title: req.params.book
+      }
+    }).then(all =>{
+      res.json(all);
+    });
+
   });
 
   // Add sequelize code to get all books of a specific genre and return them as JSON
   app.get("/api/genre/:genre", function(req, res) {
-
+    Book.findAll({
+      where: {
+        genre: req.params.genre
+      }
+    }).then(genre =>{
+      res.json(genre);
+    });
   });
 
   // Add sequelize code to get all books from a specific author and return them as JSON
   app.get("/api/author/:author", function(req, res) {
-
+    Book.findAll({
+      where: {
+        title: req.params.author
+      }
+    }).then(all =>{
+      res.json(all);
+    });
   });
 
   // Add sequelize code to get all "long" books (more than 150 pages) and return them as JSON
   app.get("/api/books/long", function(req, res) {
-
+    Book.findAll({
+      where: {
+        page: {
+          $gte: 150
+        }
+      },
+      order: "page DESC"
+    }).then(long =>{
+    res.json(long);
   });
+});
 
   // Add sequelize code to get all "short" books (150 pages or less) and return them as JSON
   app.get("/api/books/short", function(req, res) {
-
+    Book.findAll({
+      where: {
+        page: {
+          $lte: 150
+        }
+      },
+      order: "page DESC"
+    }).then(long =>{
+    res.json(long);
+  });
   });
 
   // Add sequelize code to create a book
   app.post("/api/new", function(req, res) {
-
+    Book.create({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      page: req.body.pages
+    })
   });
 
   // Add sequelize code to delete a book
   app.post("/api/delete", function(req, res) {
-
+    Book.destroy({
+      where: {
+        id: req.body.id
+      }
+    })
   });
 
 };
